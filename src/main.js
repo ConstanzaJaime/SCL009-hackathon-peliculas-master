b2ee9affc6912cfc4dd1b32aa8bb903f3c59ae47
ConstanzaJaime <constanza.jaime.a@gmail.com>
2019-05-17 13:05:08 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* Manejo del DOM */
+window.onload
+
+// fetch('https://www.omdbapi.com/?apikey=4bde1662&type=movie')
+// .then()
+// document.getElementById("root").innerHTML =
+//   `hola
+//   `;
