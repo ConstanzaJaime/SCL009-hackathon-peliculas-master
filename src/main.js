405d6d1d5c28b808ed750e1bd5942f91b0fa9fcb
Cloiw <sircli@hotmail.com>
2019-05-21 19:58:32 -0400

    index html

--- src/main.js
+++
@@ -1,8 +0,0 @@
-/* Manejo del DOM */
-window.onload
-
-// fetch('https://www.omdbapi.com/?apikey=4bde1662&type=movie')
-// .then()
-// document.getElementById("root").innerHTML =
-//   `hola
-//   `;
